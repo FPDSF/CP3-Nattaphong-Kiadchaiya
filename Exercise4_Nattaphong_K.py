c034492048f4f5ab8c34dd9df5b062e63d1b8228
FPDSF <nattaphon_g@hotmail.com>
2023-01-01 14:12:45 +0700

    Create Exercise4_Nattaphong_K.py

---
+++ Exercise4_Nattaphong_K.py
@@ -0,0 +1,9 @@
+print("---Your Score---")
+FE = 84.2
+GB = 79.5
+ICS = 76.8
+CP =92.4
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
